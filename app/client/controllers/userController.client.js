a91ea85366b1fa17a45d221ca0e8d2963b8a66cf
J. Steve Martinez <j.steve.martinez@gmail.com>
2017-01-02 16:19:51 -0800

    removed unused files

--- app/client/controllers/userController.client.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// var apiUrl = appUrl + '/api/:id';
-// // var auth;
-//
-// ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
-//   // auth = JSON.parse(data);
-//
-// }));
